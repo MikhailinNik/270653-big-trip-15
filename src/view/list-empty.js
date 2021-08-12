2c889afb85b79f24a7fd78abc6b48d9c955db184
MikhailinNik <ifyoucan1337@gmail.com>
2021-08-12 17:04:06 +0300

    Добавлен абстрактный класс; -простые классы наследуются от класса Abstract

--- src/view/list-empty.js
+++ src/view/list-empty.js
@@ -1,25 +1,9 @@
-import { createElement } from '@/utils/dom';
+import AbstractView from '@view/abstract';
 
 const createEmptyList = () => '<p class="trip-events__msg">Click New Event to create your first point</p>';
 
-export default class ListEmpty {
-  constructor() {
-    this._element = null;
-  }
-
+export default class ListEmpty extends AbstractView{
   getTemplate() {
     return createEmptyList();
-  }
-
-  getElement() {
-    if (!this._element) {
-      this._element = createElement(this.getTemplate());
-    }
-
-    return this._element;
-  }
-
-  removeElement() {
-    this._element = null;
   }
 }
